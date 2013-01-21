2060496731e9da50b444284f6ca59a153199cf98
Simon <simeks-0@student.ltu.se>
2013-01-21 16:53:33 +0100

    Dependencies added

---
+++ dependencies/install_all.bat
@@ -0,0 +1,9 @@
+cd ogre-1.7.2-r1131M-py-2.7\
+C:\Python27\python.exe setup.py install
+
+cd ..\setuptools-0.6c11\
+C:\Python27\python.exe setup.py install
+
+cd ..\openpyxl-1.6.1\
+C:\Python27\python.exe setup.py install
+
