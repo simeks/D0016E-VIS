e28e04df08c268dd04dee97164bd989dc7df58e0
Johan Forsling <ojaofi-0@student.ltu.se>
2013-03-07 15:04:42 +0100

    Added the house texture and removed the robot

--- scene.py
+++ scene.py
@@ -12,14 +12,14 @@ class Scene:
         self.root = root;
 
         # default värden
-        self.walkSpeed = 200.0 # Objektets hastighet
-        self.direction = ogre.Vector3().ZERO # Objektets riktning
-        self.distance = 0.0 # Distansen mellan objektet och punkten dit objektet är påväg
+        #self.walkSpeed = 200.0 # Objektets hastighet
+        #self.direction = ogre.Vector3().ZERO # Objektets riktning
+        #self.distance = 0.0 # Distansen mellan objektet och punkten dit objektet är påväg
 
         # 
-        self.walklist = []
-        self.walklist.append(ogre.Vector3(-4350, 0, -550))
-        self.walklist.append(ogre.Vector3(-3350, 0, 550))
+        #self.walklist = []
+        #self.walklist.append(ogre.Vector3(-4350, 0, -550))
+        #self.walklist.append(ogre.Vector3(-3350, 0, 550))
 
         self.physics = physics.PhysicsWorld();
         self.houseNumber = 0;
@@ -47,13 +47,13 @@ class Scene:
         self.physics.init(-1000);
         
         # Skapa en entitet från en mesh-fil vi har bland vår media
-        self.entity = self.sceneMgr.createEntity("Sinbad", "robot.mesh");
-        self.entity.setCastShadows(True);
+        #self.entity = self.sceneMgr.createEntity("Sinbad", "robot.mesh");
+        #self.entity.setCastShadows(True);
         # Skapa en child node till vår root node
-        self.node = self.rootNode.createChildSceneNode('SinbadNode', (-4350, 0, -550));
+        #self.node = self.rootNode.createChildSceneNode('SinbadNode', (-4350, 0, -550));
         # och fäst vår entitet vid den noden
-        self.node.attachObject(self.entity);
-        self.node.setScale(10, 10, 10);
+        #self.node.attachObject(self.entity);
+        #self.node.setScale(10, 10, 10);
         
         
         # Lägg till ett stort plan (20000x20000)
@@ -121,9 +121,9 @@ class Scene:
             self.createBarrel(2000, 320, 1300-b*200);
 
         #
-        animationState = self.entity.getAnimationState('Idle')
-        animationState.setLoop(True)
-        animationState.setEnabled(True)
+        #animationState = self.entity.getAnimationState('Idle')
+        #animationState.setLoop(True)
+        #animationState.setEnabled(True)
 
         # Lägg till ett directional light så man ser kuben något bättre
         self.light = self.sceneMgr.createLight("Light");
@@ -207,53 +207,47 @@ class Scene:
         self.barrelNumber += 1;
     
     
-    def nextLocation(self):
-      if len(self.walklist) == 0:
-         return False
-      self.destination = self.walklist.pop(0) # Plocka objektet högst upp i listan
-      self.walklist.append(self.destination) # Lägg tillbaka längst bak i listan
-      self.direction = self.destination - self.node.getPosition()
-      self.distance = self.direction.normalise()
- 
-      src = self.node.getOrientation() * ogre.Vector3().UNIT_X
-      if 1.0 + src.dotProduct(self.direction) < 0.0001:
-         self.node.yaw(ogre.Degree(180))
-      else:
-         quat = src.getRotationTo(self.direction)
-         self.node.rotate(quat)
-      return True
+##    def nextLocation(self):
+##      if len(self.walklist) == 0:
+##         return False
+##      self.destination = self.walklist.pop(0) # Plocka objektet högst upp i listan
+##      self.walklist.append(self.destination) # Lägg tillbaka längst bak i listan
+##      self.direction = self.destination - self.node.getPosition()
+##      self.distance = self.direction.normalise()
+## 
+##      src = self.node.getOrientation() * ogre.Vector3().UNIT_X
+##      if 1.0 + src.dotProduct(self.direction) < 0.0001:
+##         self.node.yaw(ogre.Degree(180))
+##      else:
+##         quat = src.getRotationTo(self.direction)
+##         self.node.rotate(quat)
+##      return True
 
 
     def frame(self, evt):
         # Kör ett steg av fysiksimuleringen
         self.physics.frame(evt);
-        if self.direction == ogre.Vector3().ZERO:
-            if self.nextLocation():
+        #if self.direction == ogre.Vector3().ZERO:
+            #if self.nextLocation():
                 # Set walking animation
-                self.animationStateTop = self.entity.getAnimationState('Walk')
-                self.animationStateTop.setLoop(True)
-                self.animationStateTop.setEnabled(True)
-                #self.animationStateBase = self.entity.getAnimationState('RunBase')
-                #self.animationStateBase.setLoop(True)
-                #self.animationStateBase.setEnabled(True)
-        else:
-            move = self.walkSpeed * evt.timeSinceLastFrame;
-            self.distance -= move
-            if self.distance <= 0.0:
-                self.node.setPosition(self.destination)
-                self.direction = ogre.Vector3().ZERO
-                if not self.nextLocation():
+                #self.animationStateTop = self.entity.getAnimationState('Walk')
+                #self.animationStateTop.setLoop(True)
+                #self.animationStateTop.setEnabled(True)
+                
+        #else:
+            #move = self.walkSpeed * evt.timeSinceLastFrame;
+            #self.distance -= move
+            #if self.distance <= 0.0:
+                #self.node.setPosition(self.destination)
+                #self.direction = ogre.Vector3().ZERO
+                #if not self.nextLocation():
                     # Set Idle animation
-                    self.animationStateTop = self.entity.getAnimationState('Idle')
-                    self.animationStateTop.setLoop(True)
-                    self.animationStateTop.setEnabled(True)
-                    #self.animationStateBase = self.entity.getAnimationState('IdleBase')
-                    #self.animationStateBase.setLoop(True)
-                    #self.animationStateBase.setEnabled(True)
-            else:
-                self.node.translate(self.direction * move)
-        self.animationStateTop.addTime(evt.timeSinceLastFrame)
-     # self.animationStateBase.addTime(evt.timeSinceLastFrame)
+                    #self.animationStateTop = self.entity.getAnimationState('Idle')
+                    #self.animationStateTop.setLoop(True)
+                    #self.animationStateTop.setEnabled(True)
+            #else:
+                #self.node.translate(self.direction * move)
+        #self.animationStateTop.addTime(evt.timeSinceLastFrame)
         
     #def shutdown(self):
         
